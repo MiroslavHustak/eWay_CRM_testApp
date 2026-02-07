e8c9275fad4d38be6b44c9b779675e9c2576215e
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2026-02-07 02:24:39 +0100

    06-02-2026

--- eWay_CRM_testApp.Models/Types/ErrorTypes.fs
+++ eWay_CRM_testApp.Models/Types/ErrorTypes.fs
@@ -2,9 +2,9 @@
 
 type [<Struct>] Errors =
     | FileNotExisting
-    | SerializationError //"Failed getting path" "Path is invalid: %s" "Kontakty nebyly uloženy."
-    | DeserializationError //"File does not exist"  "Failed to decode: %s") "Kontakty nebyly načteny."
-    | CRMError //"Nebylo možno získat data z CRM na základě příslušného emailu."
-    | ConnectionError  //"Nebylo možno získat data. Ověř připojení k CRM."
-    | UserInputError1 //"Chybný formát emailu."
-    | UserInputError2 //"Prosím zadejte platný email."
+    | SerializationError 
+    | DeserializationError 
+    | CRMError 
+    | ConnectionError  
+    | UserInputError1 
+    | UserInputError2
